57efcd95d2e3c208a744dd949e8bf7c7d3213458
Sayali Chowkekar <sayali.6119025.it@mhssce.ac.in>
2023-01-27 10:41:24 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask,render_template, request, session, redirect
 from flask_pymongo import PyMongo
 from flask_mongoengine import MongoEngine
-import bcrypt
 from flask.helpers import url_for
 
 app = Flask(__name__, template_folder='templates')
@@ -29,14 +28,19 @@ def registerhtml():
 def register_user():
     if request.method == 'POST':
         user= mongo.db.user
+        pass1 = request.form.get('password')
+        pass2 = request.form.get('cpassword')
         
         existing_user = user.find_one({'username':request.form.get('username')}) 
         if existing_user is None:
-            user.insert_one({'name': request.form.get('name'),'username' : request.form.get('username'),'dob':request.form.get('const {propertyName} = objectToDestruct;'),'phoneno' : request.form.get('phoneno'), 'password' :request.form.get('password')})
-            session['username'] = request.form['username']
+            if pass1 == pass2:
+                user.insert_one({'name': request.form.get('name'),'username' : request.form.get('username'),'dob':request.form.get('const {propertyName} = objectToDestruct;'),'phoneno' : request.form.get('phoneno'), 'password' :request.form.get('password')})
+                session['username'] = request.form['username']
+            else:
+                return "password mismatch"
         else:
             return 'username already exists'
-    return "registered"
+    return redirect(url_for('home'))
 
 @app.route('/user_login',methods=['POST','GET'])
 def candidate():
@@ -51,21 +55,7 @@ def candidate():
                 return render_template('login_success.html')
             return 'invalid password'
         return 'signup first'   
-    return "login sucessful"
-
-@app.route('/register_insert', methods=['POST'])
-def register():
-    if request.method == 'POST':
-        user= mongo.db.user
-
-        hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'),bcrypt.gensalt())
-        existing_user = user.find_one({'username':request.form.get('username')}) 
-        if existing_user is None:
-            user.insert_one({'name': request.form.get('name'),'username' : request.form.get('username'),'dob':request.form.get('const {propertyName} = objectToDestruct;'),'phoneno' : request.form.get('phoneno'), 'password' :hashpass})
-            session['username'] = request.form['username']
-        else:
-            return 'username already exists'
-    return redirect(url_for('candidate'))
+    return "login unsucessful"
 
     
 if __name__ == "__main__":
